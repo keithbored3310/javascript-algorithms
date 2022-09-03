7902dbbdfd9e7551a66ea0b91e628585f9816331
keithbored3310 <108907420+keithbored3310@users.noreply.github.com>
2022-09-03 11:49:27 +0800

    Create manhattanDistance.test.js

---
+++ src/algorithms/math/manhattan-distance/__test__/manhattanDistance.test.js
@@ -0,0 +1,7 @@
+import manhattanDistance from '../manhattanDistance';
+
+describe('manhattanDistance', () => {
+    it('should calculate the manhattan distance', () => {
+        expect(manhattanDistance(1,5,1,8)).toEqual(1);
+    })
+})
